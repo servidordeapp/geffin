47185c843c94f1dfa3611b029e0416b4ebcc1702
BdsOliveira <bds.commus@gmail.com>
2025-03-27 22:23:40 -0300

    update HasTenant trait imports

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\HasTenant;
+use App\Traits\HasTenant;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use OwenIt\Auditing\Contracts\Auditable;
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use App\HasTenant;
+use App\Traits\HasTenant;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
