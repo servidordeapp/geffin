a6ee7a8ebfc2cdb9e61d9388849fddb0f6481673
BdsOliveira <bds.commus@gmail.com>
2025-04-12 16:25:26 -0300

    add test description and one more test case

--- tests/Feature/PaymentProviders/Asaas/Subscription/CreateAsaasSubscriptionTest.php
+++ tests/Feature/PaymentProviders/Asaas/Subscription/CreateAsaasSubscriptionTest.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Exceptions\InvalidDocumentException;
 use App\Services\PaymentGateway\Providers\Asaas\Customer\AsaasCustomer;
 use App\Services\PaymentGateway\Providers\Asaas\Subscription\AsaasSubscription;
 use App\Services\PaymentGateway\Providers\Asaas\Subscription\Concerns\AsaasSubscriptionOutput;
@@ -8,43 +7,54 @@ use App\Services\PaymentGateway\Providers\Asaas\Subscription\Enums\AsaasBilingTy
 use App\Services\PaymentGateway\Providers\Asaas\Subscription\Enums\AsaasBillingCycleEnum;
 use App\Services\PaymentGateway\Utils\Trial;
 
-// it('can not create a subscription without a customer', function () {
-//     // Arrange
+describe('Create Asaas Subscription', function () {
+    
+    it('can not create a subscription without a customer', function () {
+        // Arrange
+        $subscription = [
+            'billingType' => AsaasBilingTypesEnum::PIX,
+            'value' => '100.00',
+            'nextDueDate' => new Trial,
+            'cycle' => AsaasBillingCycleEnum::MONTHLY,
+            'description' => 'Assinatura de teste',
+            'externalReference' => 'assinatura-123',
+        ];
 
-//     // Act
-//     expect(fn() => AsaasCustomer::create($customerData))
-//         ->toThrow(InvalidDocumentException::class);
-// });
+        // Act
+        expect(fn() => AsaasSubscription::create($subscription))
+            ->toThrow(InvalidArgumentException::class);
+    });
 
-it('creates a subscription to a customer in payment gateway', function () {
-    // Arrange
-    $customer = AsaasCustomer::first();
-    $subscription = [
-        'customer' => $customer->getId(),
-        'billingType' => AsaasBilingTypesEnum::PIX,
-        'value' => '100.00',
-        'nextDueDate' => new Trial,
-        'cycle' => AsaasBillingCycleEnum::MONTHLY,
-        'description' => 'Assinatura de teste',
-        'externalReference' => 'assinatura-123',
-    ];
+    it('creates a subscription to a customer in payment gateway', function () {
+        // Arrange
+        $customer = AsaasCustomer::first();
+        $subscription = [
+            'customer' => $customer->getId(),
+            'billingType' => AsaasBilingTypesEnum::PIX,
+            'value' => '100.00',
+            'nextDueDate' => new Trial,
+            'cycle' => AsaasBillingCycleEnum::MONTHLY,
+            'description' => 'Assinatura de teste',
+            'externalReference' => 'assinatura-123',
+        ];
 
-    // Act
-    $subscription = AsaasSubscription::create($subscription);
+        // Act
+        $subscription = AsaasSubscription::create($subscription);
 
-    // Assert
-    expect($subscription)
-        ->toBeInstanceOf(AsaasSubscriptionOutput::class);
-});
+        // Assert
+        expect($subscription)
+            ->toBeInstanceOf(AsaasSubscriptionOutput::class);
+    });
 
-it('throws exception when creating subscription with missing data', function () {
-    // Arrange
-    $invalidCustomerData = [
-        'name' => 'John Doe',
-        // email faltando
-    ];
+    it('throws exception when creating subscription with missing data', function () {
+        // Arrange
+        $invalidCustomerData = [
+            'name' => 'John Doe',
+            // email faltando
+        ];
 
-    // Act & Assert
-    expect(fn () => AsaasCustomer::create($invalidCustomerData))
-        ->toThrow(InvalidArgumentException::class);
+        // Act & Assert
+        expect(fn() => AsaasCustomer::create($invalidCustomerData))
+            ->toThrow(InvalidArgumentException::class);
+    });
 });
