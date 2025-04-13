9a5b80e9e91732f96bff17e31e5df47a51879102
BdsOliveira <bds.commus@gmail.com>
2025-04-13 17:23:20 -0300

    change the default layout

--- sistema/routes/web.php
+++ sistema/routes/web.php
@@ -4,12 +4,13 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
+    // return Inertia::render('Welcome');
+    return redirect()->route('dashboard');
 })->name('home');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
