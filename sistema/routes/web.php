23477f1fd5c91dce5e3910e67ea51ac49b225a50
BdsOliveira <bds.commus@gmail.com>
2025-04-15 19:07:07 -0300

    run pint

--- sistema/routes/web.php
+++ sistema/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', fn() => redirect()->route('dashboard'))->name('home');
+Route::get('/', fn () => redirect()->route('dashboard'))->name('home');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
@@ -13,5 +13,5 @@ Route::get('clientes', function () {
     return Inertia::render('Clientes');
 })->middleware(['auth', 'verified'])->name('clientes');
 
-require __DIR__ . '/settings.php';
-require __DIR__ . '/auth.php';
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
